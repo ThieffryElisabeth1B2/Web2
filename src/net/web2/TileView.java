7c3680d34fea1969d5e23f10fabc1fb43fad5e98
elisabeth.thieffry <elisabeth.thieffry@103F10.etu.iut-lens.univ-artois.fr>
2013-02-12 08:48:34 +0100

    rien

--- src/net/web2/TileView.java
+++ src/net/web2/TileView.java
@@ -180,10 +180,8 @@ public class TileView extends View {
      * @param tile
      */
     public void loadTile(int key, Drawable tile) {
-    	Resources r =this.getContext().getResources();
-    	Drawable drawable = r.getDrawable(key);
-    	int x = drawable.getIntrinsicWidth();
-    	int y = drawable.getIntrinsicHeight();
+    	int x = tile.getIntrinsicWidth();
+    	int y = tile.getIntrinsicHeight();
         Bitmap bitmap = Bitmap.createBitmap(x, y, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
         tile.setBounds(0, 0, x, y);
